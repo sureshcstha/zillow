68c879d956c6266f810374cd0c0ce3d55d69bc97
Suresh Shrestha <sureshcstha@gmail.com>
2024-01-10 23:31:30 -0700

    default layouts

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -14,9 +14,3 @@ defineProps({
 })
 </script>
 
-<script>
-import MainLayout from '../../Layouts/MainLayout.vue';
-export default {
-    layout: MainLayout
-}
-</script>
--- resources/js/Pages/Index/Show.vue
+++ resources/js/Pages/Index/Show.vue
@@ -7,10 +7,5 @@
 import { Link } from '@inertiajs/vue3';
 </script>
 
-<script>
-import MainLayout from '../../Layouts/MainLayout.vue';
-export default {
-    layout: MainLayout
-}
-</script>
+
 
