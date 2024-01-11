cc6e1f3e21cc8cea9a70465089cdf7c26f0c9dfa
Suresh Shrestha <sureshcstha@gmail.com>
2024-01-11 10:00:49 -0700

    computed data and conditional rendering

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,9 +1,25 @@
 <template>
     <Link href="/">Main Page</Link>&nbsp;
     <Link href="/show">Show Page</Link>
+    <div v-if="flashSuccess" class="success">
+        {{ flashSuccess }}
+    </div>
     <slot>Default</slot>
 </template>
 
 <script setup>
-import { Link } from '@inertiajs/vue3'
+import { computed } from 'vue'
+import { Link, usePage  } from '@inertiajs/vue3'
+
+const page = usePage()
+const flashSuccess = computed(
+    () => page.props.flash.success,
+)
 </script>
+
+<style scoped>
+.success {
+    background-color: green;
+    color: white;
+}
+</style>
