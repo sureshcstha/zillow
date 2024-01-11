15ce40f0cc0f754257839eb3a97c5da6293a1ad9
Suresh Shrestha <sureshcstha@gmail.com>
2024-01-11 09:35:23 -0700

    middlewares and sharing data with all pages

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,14 +1,9 @@
 <template>
     <Link href="/">Main Page</Link>&nbsp;
     <Link href="/show">Show Page</Link>
-    <div>The page with time {{timer}}</div>
     <slot>Default</slot>
 </template>
 
 <script setup>
 import { Link } from '@inertiajs/vue3'
-import { ref } from 'vue'
-
-const timer = ref(0)
-setInterval(() => timer.value++, 1000)
 </script>
