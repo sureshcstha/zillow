49a676630f22f6c422cd5f086b3ea4550b8139fb
Suresh Shrestha <sureshcstha@gmail.com>
2024-01-11 21:19:32 -0700

    add sign-in logic

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -1,12 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class IndexController extends Controller
 {
     public function index() {
+//         dd(Auth::user());
+         
         return inertia(
             'Index/Index',
             [
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,12 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
         \App\Models\Listing::factory(20)->create();
     }
 }
