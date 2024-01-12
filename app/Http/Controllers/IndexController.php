6a3fa664a32714da654774e0839fcef5237cc42b
Suresh Shrestha <sureshcstha@gmail.com>
2024-01-11 21:26:04 -0700

    add user login form validation

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -9,7 +9,7 @@ class IndexController extends Controller
 {
     public function index() {
 //         dd(Auth::user());
-         
+
         return inertia(
             'Index/Index',
             [
