4d23fd2d52fabcaedcab2fa397cfdb66c1e5216c
Suresh Shrestha <sureshcstha@gmail.com>
2024-01-12 17:15:11 -0700

    use null safe operator

--- app/Policies/ListingPolicy.php
+++ app/Policies/ListingPolicy.php
@@ -10,7 +10,7 @@ class ListingPolicy
 {
     public function before(?User $user, $ability)
     {
-        if ($user->is_admin /*&& $ability === 'update'*/) {
+        if ($user?->is_admin /*&& $ability === 'update'*/) {
             return true;
         }
     }
