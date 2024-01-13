1622ca7b766dbfcb5286b5318fafdabb2683a86b
Suresh Shrestha <sureshcstha@gmail.com>
2024-01-12 22:44:37 -0700

    filter form default values

--- app/Http/Controllers/RealtorListingController.php
+++ app/Http/Controllers/RealtorListingController.php
@@ -22,6 +22,7 @@ class RealtorListingController extends Controller
         return inertia(
             'Realtor/Index',
             [
+                'filters' => $filters,
                 'listings' => Auth::user()
                     ->listings()
 //                    ->mostRecent()
