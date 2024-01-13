7c27f57695ccfc84b1457b69d973f9ded2d6ae66
Suresh Shrestha <sureshcstha@gmail.com>
2024-01-13 11:17:04 -0700

    upload multiple images backend

--- app/Http/Controllers/RealtorListingImageController.php
+++ app/Http/Controllers/RealtorListingImageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use App\Models\ListingImage;
 
 class RealtorListingImageController extends Controller
 {
@@ -17,6 +18,16 @@ class RealtorListingImageController extends Controller
 
     public function store(Listing $listing, Request $request)
     {
-        dd('Works!');
+        if ($request->hasFile('images')) {
+            foreach ($request->file('images') as $file) {
+                $path = $file->store('images', 'public');
+
+                $listing->images()->save(new ListingImage([
+                    'filename' => $path
+                ]));
+            }
+        }
+
+        return redirect()->back()->with('success', 'Images uploaded!');
     }
 }
