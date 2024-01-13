36d9d464b57cb61269f4f005775278a7aa197215
Suresh Shrestha <sureshcstha@gmail.com>
2024-01-12 22:06:12 -0700

    vue watchers and debounce function

--- resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
+++ resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
@@ -4,6 +4,7 @@
             <div class="flex flex-nowrap items-center gap-2">
                 <input
                     id="deleted"
+                    v-model="filterForm.deleted"
                     type="checkbox"
                     class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
                 />
@@ -12,3 +13,21 @@
         </div>
     </form>
 </template>
+
+<script setup>
+import {reactive, watch} from 'vue';
+import {router} from '@inertiajs/vue3';
+import {debounce} from 'lodash';
+
+const filterForm = reactive({
+    deleted: false
+});
+
+watch(
+    filterForm, debounce(() => router.get(
+        route('realtor.listing.index'),
+        filterForm,
+        { preserveState: true, preserveScroll: true },
+    ), 1000),
+)
+</script>
