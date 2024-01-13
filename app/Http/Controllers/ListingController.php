ce800e88ba890b7625f1ca11bbcb5cf5c315c9b0
Suresh Shrestha <sureshcstha@gmail.com>
2024-01-12 19:26:07 -0700

    filtering javascript logic

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -16,12 +16,15 @@ class ListingController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
         return inertia(
             'Listing/Index',
             [
-                'listings' => Listing::orderByDesc('created_at')->paginate(9)
+                'filters' => $request->only([
+                    'priceFrom', 'priceTo', 'beds', 'baths', 'areaFrom', 'areaTo'
+                ]),
+                'listings' => Listing::orderByDesc('created_at')->paginate(9)->withQueryString()
             ]
         );
     }
