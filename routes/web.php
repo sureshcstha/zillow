1517459fc39df7dbbbf2ad2f1a08272d0e2811e6
Suresh Shrestha <sureshcstha@gmail.com>
2024-01-13 00:09:47 -0700

    edit, preview, and create listing

--- routes/web.php
+++ routes/web.php
@@ -23,10 +23,7 @@ Route::get('/', [IndexController::class, 'index']);
 Route::get('/show', [IndexController::class, 'show'])->middleware('auth');;
 
 Route::resource('listing', ListingController::class)
-    ->only(['create', 'store', 'edit', 'update'])
-    ->middleware('auth');
-Route::resource('listing', ListingController::class)
-    ->except(['create', 'store', 'edit', 'update', 'destroy']);
+    ->only(['index', 'show']);
 
 Route::get('login', [AuthController::class, 'create'])->name('login');
 Route::post('login', [AuthController::class, 'store'])->name('login.store');
@@ -38,6 +35,6 @@ Route::prefix('realtor')
     ->middleware('auth')
     ->group(function () {
         Route::resource('listing', RealtorListingController::class)
-            ->only(['index', 'destroy']);
+            ->only(['index', 'destroy', 'edit', 'update', 'create', 'store']);
     });
 
