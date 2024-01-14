297eb1db35e16c866e998a66e311267a2056b416
Suresh Shrestha <sureshcstha@gmail.com>
2024-01-14 10:19:28 -0700

    accept one offer, reject others

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ListingController;
 use App\Http\Controllers\ListingOfferController;
+use App\Http\Controllers\RealtorListingAcceptOfferController;
 use App\Http\Controllers\RealtorListingImageController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
@@ -49,6 +50,12 @@ Route::prefix('realtor')
         Route::resource('listing', RealtorListingController::class)
 //            ->only(['index', 'destroy', 'edit', 'update', 'create', 'store'])
             ->withTrashed();
+
+        Route::name('offer.accept')
+            ->put(
+                'offer/{offer}/accept',
+                RealtorListingAcceptOfferController::class
+            );
 
         Route::resource('listing.image', RealtorListingImageController::class)
             ->only(['create', 'store', 'destroy']);
